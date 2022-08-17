c282f750db8136bf4ed7faeb871d943ec4954f74
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-08-17 17:29:14 +0200

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Iuliia Volkova on 17.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
